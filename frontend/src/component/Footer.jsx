de177fc4b9143d9f94362e2386c89052de96e82f
Rohit Singh <rsingh.py@gmail.com>
2025-05-21 14:22:51 +0530

    text defiend

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm '  >
                 <div  >
                     <img src={assets.logo} className='mb-5 w-32 ' alt="" />
-                    <p className='w-full md:w-2/3 text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, officiis ea fugiat ratione sed, atque id saepe, vel delectus animi assumenda aspernatur. Omnis?</p>
+                    <p className='w-full md:w-2/3 text-gray-600'>Ground Floor - Asha Villa, Harhsoli, Bhowali,<br/> Nanital, Uttarakhand, India (263132)<br/> near Rajdeep General Store.</p>
                 </div>
 
                 <div>
@@ -23,7 +23,7 @@ const Footer = () => {
                 <div>
                     <p className='text-xl font-medium mb-5' >Get in Touch</p>
                     <ul className='flex flex-col gap-1 text-gray-600' >
-                        <li>+91-93939393</li>
+                        <li>+91-9393939393</li>
                         <li>mail@example.com</li>
                     </ul>
                 </div>
@@ -31,7 +31,7 @@ const Footer = () => {
 
             <div>
                 <hr />
-                <p className='py-5 text-sm text-center' >Copyright 2024 @forever.com </p>
+                <p className='py-5 text-sm text-center' >Copyright 2025 @FOREVER </p>
             </div>
         </div>
     )
